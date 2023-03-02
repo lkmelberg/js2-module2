05a6cc50de4a5760fd5f941c061b5d43ba0871e9
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-03-02 16:50:50 +0100

    some, every, object methods, chaining, best practices

--- js/lessons.js
+++ js/lessons.js
@@ -291,3 +291,175 @@ if (!foundBook) {
 }
 // Returns:
 // No books in stock!
+
+// some ()
+
+const values4 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const didMatchCondition = values4.some((value, index) => {
+  console.log({ index, value });
+  if (value >= 3) {
+    return true;
+  }
+});
+
+console.log("didMatchCondition:", didMatchCondition);
+// Return/output:
+// index: 0 value: 0
+// index: 1 value: 1
+// index: 2 value: 2
+// index: 3 value: 3
+// didMatchCondition: true
+
+// every ()
+const modules = [
+  { name: "Introduction to Programming", isComplete: true },
+  { name: "HTML", isComplete: true },
+  { name: "CSS", isComplete: false },
+  { name: "JavaScript", isComplete: true },
+];
+
+const isCourseFinished = modules.every((currentModule) => {
+  return currentModule.isComplete === true;
+});
+
+console.log(isCourseFinished);
+// Returns:
+// false
+
+// Object
+
+const person = {
+  firstName: "Ola",
+  lastName: "Nordmann",
+  module: "JavaScript",
+};
+
+Object.keys(person).forEach((key) => {
+  console.log(`key: ${key}, value: ${person[key]}`);
+});
+
+// Logs:
+// key: firstName, value: Ola
+// key: lastName, value: Nordmann
+// key: module, value: JavaScript
+
+// map example
+
+const person6 = {
+  firstName: "Ola",
+  lastName: "Nordmann",
+  module: "JavaScript",
+};
+
+// .map will return a new array that contains the same number
+// of elements as there were in the initial array
+const keysAndValues = Object.keys(person6).map((key) => {
+  return `key: ${key}, value: ${person[key]}`;
+});
+
+console.log(keysAndValues);
+// Logs:
+// 0: "key: firstName, value: Ola"
+// 1: "key: lastName, value: Nordmann"
+// 2: "key: module, value: JavaScript"
+
+console.log(Object.values(person6));
+// Returns:
+// ['Ola', 'Nordmann', 'JavaScript']
+
+Object.entries(person6).forEach((keyValuePair) => {
+  console.log(keyValuePair);
+});
+
+Object.entries(person6).forEach(([key, value]) => {
+  console.log("key:", key, "value:", value);
+});
+// Returns:
+// key: firstName value: Ola
+// key: lastName value: Nordmann
+// key: module value: JavaScript
+
+// chaining
+
+// The discount percentage being applied
+const DISCOUNT_PERCENTAGE = 20;
+
+const productss = [
+  { name: "Milk", price: 5.0, inStock: true },
+  { name: "Cheese", price: 10.0, inStock: true },
+  { name: "Bread", price: 8.0, inStock: false },
+  { name: "Beans", price: 3.0, inStock: true },
+  { name: "Eggs", price: 12.0, inStock: true },
+  { name: "Rice", price: 4.0, inStock: false },
+  { name: "Meat", price: 25, inStock: true },
+  { name: "Coffee", price: 12.0, inStock: true },
+  { name: "Tea", price: 8.0, inStock: false },
+  { name: "Bottled Water", price: 4.0, inStock: false },
+];
+
+const discountedProducts = productss
+  .filter((product) => {
+    if (product.inStock === true && product.price >= 5) {
+      return true;
+    }
+  })
+  .map((product) => {
+    product.discountedPrice =
+      product.price - (DISCOUNT_PERCENTAGE / 100) * product.price;
+    return product;
+  });
+
+console.log(discountedProducts);
+// Returns:
+// {name: 'Milk', price: 5, inStock: true, discountedPrice: 4}
+// {name: 'Cheese', price: 10, inStock: true, discountedPrice: 8}
+// {name: 'Eggs', price: 12, inStock: true, discountedPrice: 9.6}
+// {name: 'Meat', price: 25, inStock: true, discountedPrice: 20}
+// {name: 'Coffee', price: 12, inStock: true, discountedPrice: 9.6}
+
+// Additional info regarding results average
+// Typically to get an average, you perform the following calculation:
+// student average = sum of all results / total number of results
+// In this example, we perform the calculation slightly differently:
+// student average = (mark / total number of marks) + (mark / total number of marks) + ...
+// We divide each result by the total number of results and then add this to a total, giving us the same end result. We do this so we don’t have to wait until all results have been totalled before calculating the average. We can instead calculate the average when each element loops, saving us a step at the end.
+
+const students = [
+  { name: "Victoria", age: 19, isOnline: true, results: [80, 61, 66, 50, 91] },
+  { name: "Theo", age: 32, isOnline: false, results: [57, 99, 96, 72, 96] },
+  { name: "Markus", age: 54, isOnline: true, results: [50, 92, 70, 66, 90] },
+  { name: "Aksel", age: 21, isOnline: true, results: [77, 86, 70, 71, 75] },
+  { name: "Felix", age: 29, isOnline: false, results: [99, 76, 59, 74, 68] },
+  { name: "Benjamin", age: 48, isOnline: true, results: [65, 85, 74, 83, 54] },
+  { name: "Ulrik", age: 39, isOnline: true, results: [92, 72, 55, 69, 58] },
+  { name: "Alma", age: 28, isOnline: false, results: [92, 91, 66, 77, 71] },
+  { name: "July", age: 37, isOnline: true, results: [78, 70, 59, 76, 95] },
+  { name: "Leo", age: 42, isOnline: true, results: [55, 64, 88, 88, 95] },
+];
+
+const result = students
+  // Get only the students >= 30 and that are online
+  .filter((student) => {
+    if (student.age >= 30 && student.isOnline === true) {
+      return true;
+    }
+  })
+  // Reduce all of the student's averages to a class average
+  .reduce((classAverage, student, index, studentsArray) => {
+    // Reduce all of the student's results to a single average
+    const studentsTotalMarks = student.results.reduce(
+      (studentAverage, result, index, resultsArray) => {
+        studentAverage += result / resultsArray.length;
+        return studentAverage;
+      },
+      0
+    );
+    // Add the average of the student to the class average
+    classAverage += studentsTotalMarks / studentsArray.length;
+    return classAverage;
+  }, 0);
+
+console.log(result);
+// Returns:
+// 73.72
